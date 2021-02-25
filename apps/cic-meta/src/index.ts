84c1d11b48b286db45d5dc7d25059ef2cac2c9ab
Spencer Ofwiti <maxspencer56@gmail.com>
2021-02-25 09:58:43 +0000

    Add exports to interface with CICADA.

--- apps/cic-meta/src/index.ts
+++ apps/cic-meta/src/index.ts
@@ -1,5 +1,5 @@
-export { PGPSigner, PGPKeyStore, Signer } from './auth';
-export { Envelope, Syncable } from './sync';
+export { PGPSigner, PGPKeyStore, Signer, KeyStore } from './auth';
+export { ArgPair,  Envelope, Syncable } from './sync';
 export { User } from './assets/user';
 export { Phone } from './assets/phone';
 export { Config } from './config';
