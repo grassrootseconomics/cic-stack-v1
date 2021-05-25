cb239f112a18276d3bf5c1c2df0d677154724035
Blair Vanderlugt <blair.vanderlugt@gmail.com>
2021-05-25 16:22:26 +0000

    Unit tests for cic-eth

--- apps/cic-eth/setup.py
+++ apps/cic-eth/setup.py
@@ -11,17 +11,6 @@ while True:
     requirements.append(l.rstrip())
 f.close()
 
-test_requirements = []
-f = open('test_requirements.txt', 'r')
-while True:
-    l = f.readline()
-    if l == '':
-        break
-    test_requirements.append(l.rstrip())
-f.close()
-
-
 setup(
-    install_requires=requirements,
-    tests_require=test_requirements,
+    install_requires=requirements
         )
