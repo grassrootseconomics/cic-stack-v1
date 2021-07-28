0d6e228f8afaf7ed788eb6d67da2b4c0749f2304
Blair Vanderlugt <blair.vanderlugt@gmail.com>
2021-07-28 23:33:34 +0000

    migrations fix

--- apps/cic-notify/docker/db.sh
+++ apps/cic-notify/docker/db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-migrate.py -c /usr/local/etc/cic-notify --migrations-dir /usr/local/share/cic-notify/alembic -vv
+python migrate.py -c /usr/local/etc/cic-notify --migrations-dir /usr/local/share/cic-notify/alembic -vv
