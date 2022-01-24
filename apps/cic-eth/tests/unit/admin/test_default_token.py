0e0380b1fec7544ebc79a558dcd52a6bd7307c43
Louis Holbrook <accounts-gitlab@holbrook.no>
2022-01-24 14:06:43 +0000

    bug, test: Add default token data in test fixture

--- apps/cic-eth/tests/unit/admin/test_default_token.py
+++ apps/cic-eth/tests/unit/admin/test_default_token.py
@@ -1,12 +1,26 @@
 # external imports
 import celery
+from chainlib.eth.address import is_same_address
+from cic_eth_registry.pytest.fixtures_tokens import *
+
+# local imports
+from cic_eth.task import BaseTask
+
+
+
+def test_default_token_basetask(
+    default_token,
+        ):
+
+    assert is_same_address(BaseTask.default_token_address, default_token)
+    assert BaseTask.default_token_decimals == 6
+    assert BaseTask.default_token_symbol == 'FOO'
+    assert BaseTask.default_token_name == 'Foo Token'
 
 
 def test_default_token(
         default_token,
         celery_session_worker,
-        foo_token,
-        foo_token_symbol,
         ):
       
     s = celery.signature(
@@ -17,5 +31,7 @@ def test_default_token(
     t = s.apply_async()
     r = t.get()
 
-    assert r['address'] == foo_token
-    assert r['symbol'] == foo_token_symbol
+    assert r['address'] == default_token
+    assert r['decimals'] == 6
+    assert r['name'] == 'Foo Token'
+    assert r['symbol'] == 'FOO'
