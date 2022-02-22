7f0475da4252509e2f30b832541d0967d76e34c3
semvervot <semverbot@grassroots.org>
2022-02-22 10:49:50 -0600

    fix: update the uwsgi configs for ussd

--- apps/cic-ussd/docker/start_cic_user_server.sh
+++ apps/cic-ussd/docker/start_cic_user_server.sh
@@ -4,4 +4,5 @@
 
 user_server_port=${SERVER_PORT:-9500}
 
-/usr/local/bin/uwsgi --ini ./docker/uwsgi.ini --wsgi-file cic_ussd/runnable/daemons/cic_user_server.py --stats 0.0.0.0:5050 --stats-http --http-socket :"$user_server_port" --pyargv "$@" 
+/usr/local/bin/uwsgi --ini ./docker/uwsgi.ini --wsgi-file cic_ussd/runnable/daemons/cic_user_server.py --stats 0.0.0.0:5050 --stats-http --http :"$user_server_port" --pyargv "$@" 
+
--- apps/cic-ussd/docker/start_cic_user_ussd_server.sh
+++ apps/cic-ussd/docker/start_cic_user_ussd_server.sh
@@ -4,4 +4,4 @@
 
 user_ussd_server_port=${SERVER_PORT:-9000}
 
-/usr/local/bin/uwsgi --ini ./docker/uwsgi.ini --wsgi-file cic_ussd/runnable/daemons/cic_user_ussd_server.py --stats 0.0.0.0:5050 --stats-http --http-socket :"$user_ussd_server_port" --pyargv "$@"
+/usr/local/bin/uwsgi --ini ./docker/uwsgi.ini --wsgi-file cic_ussd/runnable/daemons/cic_user_ussd_server.py --stats 0.0.0.0:5050 --stats-http --http :"$user_ussd_server_port" --pyargv "$@"
