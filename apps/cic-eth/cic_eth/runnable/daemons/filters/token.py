32b041a7474b0e58a0c8c64f3cdce46585344337
lash <dev@holbrook.no>
2022-07-08 07:13:40 +0000

    Catch no contract error in right place

--- apps/cic-eth/cic_eth/runnable/daemons/filters/token.py
+++ apps/cic-eth/cic_eth/runnable/daemons/filters/token.py
@@ -42,7 +42,12 @@ class TokenFilter(SyncFilter):
             return None
 
         token_address = tx.inputs[0]
-        token = ERC20Token(self.chain_spec, conn, token_address)
+        token = None
+        try:
+            token = ERC20Token(self.chain_spec, conn, token_address)
+        except NotAContractError:
+            logg.error('alleged erc20 token at address {} is not a contraact, skipping'.format(token_address))
+            return None
 
         registry = CICRegistry(self.chain_spec, conn)
         r = None
@@ -51,9 +56,7 @@ class TokenFilter(SyncFilter):
         except UnknownContractError:
             logg.debug('token {} not in registry, skipping'.format(token.symbol))
             return None
-        except NotAContractError:
-            logg.error('alleged erc20 token at address {} is not a contraact, skipping'.format(token_address))
-            return None
+        
 
         if is_same_address(r, ZERO_ADDRESS):
             return None
