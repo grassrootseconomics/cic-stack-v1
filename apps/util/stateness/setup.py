55d889a14c4f8b239788c836d44b00f535094d3a
Mohammed Sohail <sohailsameja@gmail.com>
2022-11-10 11:31:18 +0300

    chore: rehab stateness
    * move to https://github.com/grassrootseconomics/stateness

--- apps/util/stateness/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-setup(
-        name='stateness',
-        version='0.0.1a1',
-        packages=['stateness'],
-        include_package_data=True,
-        )
--- apps/util/stateness/stateness/base.py
+++
@@ -1,9 +0,0 @@
-class Monitor:
-
-    def __init__(self, domain):
-        self.domain = domain
-        self.u = []
-        self.persist = []
-        self.connect()
-        self.lock()
-        self.load()
